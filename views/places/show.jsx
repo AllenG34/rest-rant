237d64b4118a0d4ad497c7ace72fcae7dba6cc86
Allen Go <goallen2001@gmail.com>
2023-05-03 13:21:04 -0700

    almost done need to fix columns

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,11 +5,20 @@ function show(data) {
   return (
     <Def>
       <main>
-        <h1>{data.place.name}</h1>
+      <div className="row">
+      <div className="col-sm-6">
+        <img src={data.place.pic} alt ={data.place.name}/>
+      </div>
+      <div className="col-sm-6">
+      <h1>{data.place.name}</h1>
         <h2>Rating</h2>
         <p>Not Rated</p>
         <h2>Description</h2>
-        <p>No</p>
+        <p>{data.place.showEstablished()}</p>
+        <h3>Located in {data.place.city}, {data.place.state}</h3>
+        <h4>Serving {data.place.cuisines}</h4>
+      </div>
+      </div>
         <a href={`/places/${data.id}/edit`} className="btn btn-warning">
           Edit
         </a>
